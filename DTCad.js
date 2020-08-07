861032b7f0ce7b5383908013300ccd1bf239a112
NinaKoo <n.koohatammakun@gmail.com>
2020-08-08 00:31:48 +0700

    upload

--- DTCad.js
+++ DTCad.js
@@ -1,5 +1,14 @@
-<script>  
-function displayDate(1596126912) {
-  document.getElementById("showDate").innerHTML = Date();
+<script>
+    
+function calcTime(city, offset) {
+    d = new Date();
+    utc = d.getTime() + (d.getTimezoneOffset() * 60000);
+    nd = new Date(utc + (3600000*offset));
+    return "The local time in " + city + " is " + nd.toLocaleString();
+    document.getElementById("new Date").innerHTML = "(calcTime('Canada', '-4'))";
 }
-</script> 
+
+// get Canada
+alert(calcTime('Canada', '-4'));
+
+</script>
