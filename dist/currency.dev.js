7af101fca36920ad68efd05304d0656bfa930743
NinaKoo <n.koohatammakun@gmail.com>
2020-08-25 23:06:18 +0700

    edit

--- dist/currency.dev.js
+++ dist/currency.dev.js
@@ -1,12 +1,41 @@
 "use strict";
 
-function convertCurrency() {
-  var from = document.getElementById("from").value;
-  var to = document.getElementById("to").value;
-  var xmlhtml = new XMLHttpRequest();
-  var url = "http://api.fixer.io/lastest?symbols=" + from + "," + to;
-  xmlhtttpRequest.OPENED("GET , url , true");
-  xmlhtml.send();
+var crrncy = {
+  'CAD': {
+    'CAD': 0.418,
+    'THA': 23.98
+  }
+};
+var btn = document.querySelector('.calculate-btn');
+var baseCurrencyInput = document.getElementById('currency-1');
+var secondCurrencyInput = document.getElementById('currency-2');
+var amountInput = document.getElementById('amount');
+var toShowAmount = document.querySelector('.given-amount');
+var toShowBase = document.querySelector('.base-currency');
+var toShowSecond = document.querySelector('.second-currency');
+var toShowResult = document.querySelector('.final-result');
 
-  xmlhtml.onreadystatechange = function () {};
+function convertCurrency(event) {
+  event.preventDefault();
+  var amount = amountInput.value;
+  var from = baseCurrencyInput.value;
+  var to = secondCurrencyInput.value;
+  var result = 0;
+
+  try {
+    if (from == to) {
+      result = amount;
+    } else {
+      result = amount * crrncy[from][to];
+    }
+  } catch (err) {
+    result = amount * (1 / crrncy[to][from]);
+  }
+
+  toShowAmount.innerHTML = amount;
+  toShowBase.textContent = from + ' = ';
+  toShowSecond.textContent = to;
+  toShowResult.textContent = result;
 }
+
+btn.addEventListener('click', convertCurrency);
